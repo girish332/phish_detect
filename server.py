9090451de42172318d0e3552e7dd4f9fc817ecb4
girish332 <kulkarni.girish332@gmail.com>
2019-06-18 22:34:45 +0800

    Added the ipython notebook and the server file

---
+++ server.py
@@ -0,0 +1,16 @@
+
+import os
+import numpy as np
+import flask
+import pickle
+from flask import Flask, render_template, request
+
+#creating instance of the class
+app=Flask(__name__)
+
+#to tell flask what url shoud trigger the function index()
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+    
