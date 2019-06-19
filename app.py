8a439838df19011d7b7060129b0d22c68ea710db
girish332 <kulkarni.girish332@gmail.com>
2019-06-19 23:48:00 +0800

    Added the models and initialised the html pages and changed app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask,render_template,url_for,request
+from flask_bootstrap import Bootstrap 
+import pandas as pd 
+import numpy as np 
+
+# ML Packages
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.externals import joblib
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	df= pd.read_csv("phishcoop.csv")
+	# Features and Labels
+	df_X = df.iloc[::-1]
+	df_Y = df.iloc[:-1]
+    
+    # # Vectorization
+	# corpus = df_X
+	# cv = CountVectorizer()
+	# X = cv.fit_transform(corpus) 
+	
+	# Loading our ML Model
+	xgboost_model = open("xgboost.pkl","rb")
+	clf = joblib.load(xgboost_model)
+
+	# Receives the input query from form
+	if request.method == 'POST':
+		namequery = request.form['namequery']
+		# data = [namequery]
+		# vect = cv.transform(data).toarray()
+		# my_prediction = clf.predict(vect)
+	return render_template('results.html')
+
+    #return render_template('results.html',prediction = my_prediction,name = namequery.upper())
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
